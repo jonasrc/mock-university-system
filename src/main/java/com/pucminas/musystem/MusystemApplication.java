eeeb1b14fd1368f746412458f432eabd4a285a8e
Henrique Garretto <henrique.s.garretto@accenture.com>
2020-05-12 20:25:51 -0300

    Initial commit - project basic structure creation

---
+++ src/main/java/com/pucminas/musystem/MusystemApplication.java
@@ -0,0 +1,13 @@
+package com.pucminas.musystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pucminas/musystem/MusystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pucminas.musystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
